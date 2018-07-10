ffbc8bf1fd8921d3e587527200757c64f4d4bb0b
Henry <henry81325@gmail.com>
2018-07-10 12:01:01 +0800

    add favorite / unfavorite action on RestaurantsController

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -19,6 +19,22 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  # POST /restaurants/:id/favorite
+  def favorite
+    @restaurant = Restaurant.find(params[:id])
+    Favorite.create(restaurant: @restaurant, user: current_user)
+    redirect_back(fallback_location: root_path)  # 導回上一頁
+  end
+
+  # POST /restaurants/:id/unfavorite
+  # where 搭配 destroy.all，以刪除 where 回傳的物件集合
+  def unfavorite
+    @restaurant = Restaurant.find(params[:id])
+    favorites = Favorite.where(restaurant: @restaurant, user: current_user)
+    favorites.destroy_all
+    redirect_back(fallback_location: root_path)
+  end
+
 end
 
 
